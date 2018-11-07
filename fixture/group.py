2becf1cc141e158fbb9cf93b627b50f872bacc19
Kalinin Oleg <okmail.inbox@gmail.com>
2018-11-08 01:04:08 +0700

    Домашнее задание №6: Сделать фикстуру общей для всех тестов + test_delete_first_group

--- fixture/group.py
+++ fixture/group.py
@@ -5,6 +5,15 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
     def create(self, Group):
         wd = self.app.wd
         self.open_groups_page()
