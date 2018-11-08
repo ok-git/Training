2508976091ee251482161a9d561c94094c9c93e6
Kalinin Oleg <okmail.inbox@gmail.com>
2018-11-08 18:33:17 +0700

    Добавил в проект edit_first_contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -5,6 +5,64 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
+    def edit_first_contact(self, Contact):
+        wd = self.app.wd
+        # init edition
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(Contact.firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(Contact.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(Contact.lastname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(Contact.title)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(Contact.company)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(Contact.address)
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(Contact.home)
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(Contact.mobile)
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(Contact.work)
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(Contact.fax)
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(Contact.email)
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(Contact.email2)
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(Contact.email3)
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(Contact.homepage)
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(Contact.bday)
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(Contact.bmonth)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(Contact.byear)
+        wd.find_element_by_name("aday").click()
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(Contact.aday)
+        wd.find_element_by_name("amonth").click()
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(Contact.amonth)
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(Contact.ayear)
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(Contact.address2)
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(Contact.phone2)
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(Contact.notes)
+        # accept contact edition
+        wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
+
     def delete_first_contact(self):
         wd = self.app.wd
         #select first contact
@@ -69,5 +127,5 @@ class ContactHelper:
         wd.find_element_by_name("phone2").send_keys(Contact.phone2)
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(Contact.notes)
-        # submit group creation
+        # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
---
+++ test/edit_contact_test.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+def test_edit_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(Contact(
+                     firstname="Василий",
+                     middlename="Васильевич",
+                     lastname="Васильев",
+                     title="Бухгалтер",
+                     company="ООО Тоже Хороший софт",
+                     address="г. Город-2, ул. Улица-2, д. 2, оф. 306",
+                     home="9234567",
+                     mobile="9345678",
+                     work="9456789",
+                     fax="9567890",
+                     email="email@2goodsoft.ru",
+                     email2="pr@2goodsoft.ru",
+                     email3="sales@2goodsoft.ru",
+                     homepage="www.2goodsoft.ru",
+                     bday="2",
+                     bmonth="May",
+                     byear="1971",
+                     aday="5",
+                     amonth="June",
+                     ayear="2012",
+                     address2="г. Город-2, ул. Улица_3, д. 3, оф. 2",
+                     phone2="9122334455",
+                     notes="Это редакция контакта"))
+    app.session.logout()
