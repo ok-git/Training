4b4dc81192b2df544495b9f74e48b371a589597e
Kalinin Oleg <okmail.inbox@gmail.com>
2018-11-22 00:22:29 +0700

    Реализована проверка в виде сравнения списков для групп

--- test/edit_group_test.py
+++ test/edit_group_test.py
@@ -4,5 +4,11 @@ from model.group import Group
 def test_edit_name_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Test Name", footer="Test Footer"))
-    app.group.edit_first_group(Group(name="Change Name Only"))
-
+    old_groups = app.group.get_group_list()
+    group = Group(name="Change Name Only")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
