04894104d87f46274b65b8dd4599348aef5d62fb
Kalinin Oleg <okmail.inbox@gmail.com>
2018-11-24 01:00:56 +0700

    Домашнее задание №11: Реализовал проверки в виде сравнения списков для тестов с группами и контактами

--- test/edit_contact_test.py
+++ test/edit_contact_test.py
@@ -7,9 +7,15 @@ def test_edit_firstname_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(
             firstname="Петр",
-            middlename="Петрович",
             lastname="Петров"))
-    app.contact.edit_first_contact(Contact(firstname="Василий"))
+    old_contacts = app.contact.get_contacts_list()
+    contact = Contact(firstname="Василий", lastname="Васильев")
+    contact.id = old_contacts[0].id
+    app.contact.edit_first_contact(contact)
+    new_contacts = app.contact.get_contacts_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 #def test_modify_group_membership_for_all_contacts(app):
