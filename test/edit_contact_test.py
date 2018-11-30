7926dd5f2260ae3839139574cf8d064345d2b223
Kalinin Oleg <okmail.inbox@gmail.com>
2018-12-01 01:35:31 +0700

    Домашнее задание №16: реализовал опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения +доработка

--- test/edit_contact_test.py
+++ test/edit_contact_test.py
@@ -8,10 +8,11 @@ def test_edit_some_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(
             firstname="Петр",
-            lastname="Петров"))
+            lastname="Петров"),
+            address="г. Город, ул. Улица, д. 1, оф. 305")
     old_contacts = app.contact.get_contacts_list()
     index = randrange(len(old_contacts))
-    contact = Contact(firstname="Андрей", lastname="Андреев")
+    contact = Contact(firstname="Андрей", lastname="Андреев", address="г. Город, ул. Улица, д. 1, оф. 305")
     contact.id = old_contacts[index].id
     app.contact.edit_contact_by_index(index, contact)
     new_contacts = app.contact.get_contacts_list()
