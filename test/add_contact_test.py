42ffb62a359d4e374277fd0ca102b2546021fdda
Oleg <olka.contact@gmail.com>
2018-12-05 12:52:45 +0700

    Динамическая генерация тестов для контактов

--- test/add_contact_test.py
+++ test/add_contact_test.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-from data.contacts import constant as testdata
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, data_contacts):
+    contact = data_contacts
     old_contacts = app.contact.get_contacts_list()
     app.contact.create(contact)
     new_contacts = app.contact.get_contacts_list()
