eaf9ace52b6e25edbdfae2b87f48bd57844f83e6
Oleg <olka.contact@gmail.com>
2018-12-05 18:15:55 +0700

    Домашнее задание №19: реализована загрузка тестовых данных из файлов json

--- test/add_group_test.py
+++ test/add_group_test.py
@@ -2,8 +2,9 @@
 from model.group import Group
 
 
-def test_add_group(app, data_groups):
-    group = data_groups
+# another value - data_groups
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     new_groups = app.group.get_group_list()
