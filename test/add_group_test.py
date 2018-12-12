758fbdd42b58376ff56441e5bbe68ee4adf20dcb
Oleg <olka.contact@gmail.com>
2018-12-12 18:46:30 +0700

    Перевёл del_group_test на запрос из БД

--- test/add_group_test.py
+++ test/add_group_test.py
@@ -8,6 +8,6 @@ def test_add_group(app, db, json_groups):
     old_groups = db.get_group_list()
     app.group.create(group)
     new_groups = db.get_group_list()
-    assert len(old_groups)+1 == len(new_groups)
+    # assert len(old_groups)+1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
