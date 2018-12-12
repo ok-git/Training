396f7607cbddf1a430fe4ffac51a216a6f85bfe3
Oleg <olka.contact@gmail.com>
2018-12-12 19:49:58 +0700

    Параметр check_ui для del_group_test

--- test/del_group_test.py
+++ test/del_group_test.py
@@ -2,7 +2,7 @@ from model.group import Group
 import random
 
 
-def test_delete_some_group(app, db):
+def test_delete_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="Test Name"))
     old_groups = db.get_group_list()
@@ -14,3 +14,5 @@ def test_delete_some_group(app, db):
     # old_groups[index:index+1] = []
     old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
