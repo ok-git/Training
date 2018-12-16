9cba9c78d573b7900e47914a6b94ecacd5babf3a
Kalinin Oleg <okmail.inbox@gmail.com>
2018-12-16 17:08:20 +0700

    Домашнее задание №21: переделал тесты для проверки информации о контактах на главной странице, теперь сравнивается весь список контактов с данными из БД

--- fixture/db.py
+++ fixture/db.py
@@ -28,10 +28,22 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname, address from addressbook where deprecated='0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, email, email2, email3, phone2\
+             from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname, address) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address))
+                (id, firstname, lastname, address, home, mobile, work, email, email2, email3, phone2) = row
+                list.append(Contact(id=str(id),
+                                    firstname=firstname,
+                                    lastname=lastname,
+                                    address=address,
+                                    home=home,
+                                    mobile=mobile,
+                                    work=work,
+                                    email=email,
+                                    email2=email2,
+                                    email3=email3,
+                                    phone2=phone2
+                                    ))
         finally:
             cursor.close()
         return list
--- test/contact_fields_test.py
+++ test/contact_fields_test.py
@@ -3,7 +3,7 @@ from random import randrange
 from model.contact import Contact
 
 
-def test_contact_fields_comparison(app):
+def test_contact_fields_comparison(app, db, check_ui):
     if app.contact.count() == 0:
         app.contact.create(Contact(
             firstname="Петр",
@@ -15,14 +15,21 @@ def test_contact_fields_comparison(app):
             email="email@goodsoft.ru",
             email2="pr@goodsoft.ru",
             email3="sales@goodsoft.ru"))
-    index = randrange(app.contact.count())
-    contact_from_home_page = app.contact.get_contacts_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_phones_from_homepage == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_homepage == merge_emails_like_on_home_page(contact_from_edit_page)
+    contacts_from_home_page = app.contact.get_contacts_list()
+    contacts_from_db = db.get_contacts_list()
+    for contact in contacts_from_db:
+        contact.all_phones_from_homepage = merge_phones_like_on_home_page(contact)
+        contact.all_emails_from_homepage = merge_emails_like_on_home_page(contact)
+    assert sorted(contacts_from_home_page, key=Contact.id_or_max) == sorted(contacts_from_db, key=Contact.id_or_max)
+    if check_ui:
+        index = randrange(app.contact.count())
+        contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+        contact_from_home_page = app.contact.get_contacts_list()[index]
+        assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+        assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+        assert contact_from_home_page.address == contact_from_edit_page.address
+        assert contact_from_home_page.all_phones_from_homepage == merge_phones_like_on_home_page(contact_from_edit_page)
+        assert contact_from_home_page.all_emails_from_homepage == merge_emails_like_on_home_page(contact_from_edit_page)
 
 
 def clear(s):
