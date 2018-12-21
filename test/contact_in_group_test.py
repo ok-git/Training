70c8967714da7ae34e2b08a8b63e0d8d03c12c58
Kalinin Oleg <okmail.inbox@gmail.com>
2018-12-21 23:50:03 +0700

    Домашнее задание №22 - дополнение 2 по предусловиям

--- test/contact_in_group_test.py
+++ test/contact_in_group_test.py
@@ -28,7 +28,9 @@ def test_del_some_contact_from_some_group(app, db, ormdb):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="Test Group to add Contact"))
     if len(ormdb.get_groups_with_contacts()) == 0:
-        test_add_some_contact_to_some_group(app, db, ormdb)
+        contact = random.choice(db.get_contacts_list())
+        group = random.choice(db.get_group_list())
+        app.contact.add_contact_to_group_by_id(contact, group)
     group_to_disconnect = random.choice(ormdb.get_groups_with_contacts())
     old_contacts_in_group = ormdb.get_contacts_in_group(group_to_disconnect)
     contact_to_disconnect = random.choice(old_contacts_in_group)
